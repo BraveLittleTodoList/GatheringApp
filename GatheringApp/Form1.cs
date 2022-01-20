173ca100fe6767698153bf09342fc8b08d8c82bc
Carlos Chapa <bravelittletodolist@gmail.com>
2022-01-20 13:33:39 -0700

    Refctored getArea in to a method and placed it into cbGatheringSelect

--- GatheringApp/Form1.cs
+++ GatheringApp/Form1.cs
@@ -28,37 +28,54 @@ namespace GatheringApp
 
         }
 
-        private void cbGatheringSelect_SelectedIndexChanged(object sender, EventArgs e)
+        public void getArea()
         {
-            int selct = cbGatheringSelect.SelectedIndex;
-            switch (selct)
+            int area = cbGatheringSelect.SelectedIndex;
+            string currentArea;
+            switch (area)
             {
                 case 0:
                     pbGatheringImage.Image = Image.FromFile(@"C:\Users\cchap\source\repos\GatheringApp\GatheringApp\img\Forest.jpg");
+                    currentArea = "Forest";
                     break;
                 case 1:
                     pbGatheringImage.Image = Image.FromFile(@"C:\Users\cchap\source\repos\GatheringApp\GatheringApp\img\Desert.png");
+                    currentArea = "Desert";
                     break;
                 case 2:
                     pbGatheringImage.Image = Image.FromFile(@"C:\Users\cchap\source\repos\GatheringApp\GatheringApp\img\Grasslands.jpg");
+                    currentArea = "Grasslands";
                     break;
                 case 3:
                     pbGatheringImage.Image = Image.FromFile(@"C:\Users\cchap\source\repos\GatheringApp\GatheringApp\img\Marsh.jpg");
+                    currentArea = "Marsh";
                     break;
                 case 4:
                     pbGatheringImage.Image = Image.FromFile(@"C:\Users\cchap\source\repos\GatheringApp\GatheringApp\img\Mountain.jpg");
+                    currentArea = "Mountain";
                     break;
             }
-                 
         }
 
-      
+        private void cbGatheringSelect_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            getArea();
+        }
+         
 
+      
+        
         private void button1_Click(object sender, EventArgs e)
         {
+          
             Random rnd = new Random();
             int roll = rnd.Next(1, 100);
             infoLog.Text = roll.ToString();
+
+
+            
+            
+
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
